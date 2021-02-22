6c3fe9d679b8f3dfbe01d23826920bc20d898240
Mislah <info.mislah@gmail.com>
2021-02-22 13:07:59 +0530

    feat: Add the About menu
    An About menu has been added, providing information about the author and
    the copyright details, including a preamble of the GPL 3.0 license.

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 /*
- * Sudoku 4.0.2
+ * Sudoku 4.1.0
  * Copyright 2021 Mislah Rahman.
  * Author: Mislah Rahman
  *
@@ -437,8 +437,27 @@ void about(void) {
 	char c;
 	fflush(stdout);
 	system("clear");
-	printf("\n Sudoku v4.0.2\n\n Developed by Mislah Rahman.\n");
-	printf("\n Press q to quit : ");
+	printf("\n\
+    Sudoku v4.1.0\n\
+    \n\
+    Copyright 2021 Mislah Rahman.\n\
+    Author: Mislah Rahman\n\
+    License: GNU GPL 3.0 or later\n\
+    \n\
+    This program is free software: you can redistribute it and/or modify\n\
+    it under the terms of the GNU General Public License as published by\n\
+    the Free Software Foundation, either version 3 of the License, or\n\
+    (at your option) any later version.\n\
+    \n\
+    This program is distributed in the hope that it will be useful,\n\
+    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+    GNU General Public License for more details.\n\
+    \n\
+    You should have received a copy of the GNU General Public License\n\
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.\n\
+    \n\
+    ");
 	fflush(stdout);
 	read(STDIN_FILENO, &c, 1);
 }
